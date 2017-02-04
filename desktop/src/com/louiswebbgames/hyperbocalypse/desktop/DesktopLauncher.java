1d6ce0403d20472038926d3603106ab9a79f7854
Louis <louis.webb5@gmail.com>
2017-02-04 15:22:44 -0800

    Initial commit of empty gdx project

---
+++ desktop/src/com/louiswebbgames/hyperbocalypse/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.louiswebbgames.hyperbocalypse.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.louiswebbgames.hyperbocalypse.HyperbocalypseGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HyperbocalypseGame(), config);
+	}
+}
